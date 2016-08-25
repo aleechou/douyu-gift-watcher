fd55216f8680e264ac317de0aea45ff80b9da556
alee <alee@chou.im>
2016-08-25 19:50:39 +0800

    modify

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ setInterval(function(){
 var $output = jQuery(
     "<div style='position:fixed; top:50px; left:10px; width: 300px; height: 400px; background-color:#eee; z-index: 1000;font-Size:18px;background:url(http://ww3.sinaimg.cn/mw690/006qyfFOgw1f75k182pxlj30qo0qowjq.jpg) no-repeat;'>"
         + "<span giftname='赞'>0</span>"
+        + "<span giftname='100鱼丸'>0</span>"
         + "<span giftname='飞机'>0</span>"
+
         + "</div>"
 ).appendTo(document.body)
 
